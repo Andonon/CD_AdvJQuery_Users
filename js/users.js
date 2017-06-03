68dd188ddebad0a95a5fa04c223ee570f0a36d52
Troy Center <troycenter1@gmail.com>
2017-06-03 11:20:57 -0700

    finsihed js file, tested, and W3C validation. Done with project, merging to master.

--- js/users.js
+++ js/users.js
@@ -1,5 +1,29 @@
 "use strict";
     $(document).ready(function(){ 
-        
+        $("#formsubmitbutton").click(function(){
+            $("#userlist tbody:last").append("<tr></tr>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("firstname").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("lastname").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("email").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("phone").value+"</td>");
+            // $("#userlist tr:last").after.append("</tr>");
+            // alert("Added User");  
+            var firstname = document.getElementById("firstname").value
+            var lastname = document.getElementById("lastname").value
+            success([firstname+" "+lastname]);        
+        });
     });    
-    
+    function formChanged(){
+        var firstname = document.getElementById("firstname").value;
+            // console.log(firstname);
+        var lastname = document.getElementById("lastname").value;
+            // console.log(lastname);
+        var email = document.getElementById("email").value;
+            // console.log(email);
+        var phone = document.getElementById("phone").value;
+            // console.log(phone);
+    }
+    function success(output){
+        alert("User Added: "+output);
+        $("#form1").reset();
+    }
